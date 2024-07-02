86a555f81b248f75507248c779d0531903c7832a
Fernando Mumbach <fermuch@ayvu.net>
2024-07-02 09:28:33 -0300

    added globals and pplus into test_helpers

--- src/helpers/test_helpers.lua
+++ src/helpers/test_helpers.lua
@@ -9,3 +9,47 @@ _G.json = {
     return JSON:encode(str)
   end,
 }
+
+_G.globals = {
+  id = function ()
+    return "test-id"
+  end,
+
+  runid = function ()
+    return "test-run-id"
+  end,
+
+  print = _G.print,
+}
+
+_G.pplus = {
+  set_script_metadata = function(metadata)
+    return true, nil
+  end,
+
+  get_script_metadata = function ()
+    return {
+      name = "test",
+      version = "0.0.1",
+      description = "Test helpers package",
+      settings_form = nil,
+      settings_schema = nil
+    }
+  end,
+
+  get_settings = function ()
+    return nil
+  end,
+
+  get_token = function ()
+    return nil
+  end,
+
+  get_api = function ()
+    return "https://pagoplus.fly.dev/api/graphql"
+  end,
+
+  execute = function ()
+    return "Not implemented for tests", nil
+  end
+}
